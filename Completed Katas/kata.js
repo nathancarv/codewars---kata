8c497fcd3d34a2819f92de12cea086eb15ce3bb8
Nathaniel Carvajal <nathanielcarvajal@gmail.com>
2019-06-17 19:48:39 -0400

    L1 set alarm

--- Completed Katas/kata.js
+++ Completed Katas/kata.js
@@ -113,20 +113,26 @@ function sum(numbers) {
   return counter;
 }
 
-
 //Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right
 
 //Instructions: Remove n exclamation marks in the sentence from left to right. n is positive integer.
 
 //Answer:
-function remove(s,n){
-  for (let i = 1; i <= n; i++){
-  console.log(i)
-  s = s.replace('!', '');
-  console.log(s)
+function remove(s, n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+    s = s.replace("!", "");
+    console.log(s);
   }
   return s;
-  
 }
 
+//L1: Set Alarm
 
+//Instructions: Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+//The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise
+
+//Answer:
+function setAlarm(employed, vacation) {
+  return employed && !vacation;
+}
